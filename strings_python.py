72d2272937b3856fc5115697dcf0f788f7b3580c
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-14 12:18:53 -0600

    accept input

--- strings_python.py
+++ strings_python.py
@@ -4,3 +4,7 @@ print("snek's are easy to use")
 print('can us quotes "in" singles')
 # concantants
 print("hello" + " sneks")
+
+greet = "Hello"
+name = input("Please enter your name:")
+print (greet + ' ' + name)
