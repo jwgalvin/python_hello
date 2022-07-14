a971c9f337f6729d15a67671f60608965f33ab19
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-14 12:29:18 -0600

    to use same quote in and out

---
+++ escape_char.py
@@ -0,0 +1,7 @@
+splitString = "This string \nsplit over\ndifferent\nlines."
+print(splitString)
+tabbed = "1\t2\t3\t4"
+print(tabbed)
+
+print('the pet show "word, \'e\'s resting".')
+print("the pet show \"word, 'e's resting\".")
