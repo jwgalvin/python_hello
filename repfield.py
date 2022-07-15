6716ad3fc794f0c8db248410b3e91699722ce931
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-15 16:06:04 -0600

    string magic

---
+++ repfield.py
@@ -0,0 +1,13 @@
+age = 43
+print("My age is " + str(age) + " years")
+print()
+print("My age is {0} years".format(age))
+
+print("There are {0} days in {1}, {2}, {3}, {4}, {5}, {6} and {7}"
+    .format(31, "Jan", "Mar", "May", "Jul", "Aug", "Oct", "Dec"))
+    
+print()
+
+# print("""
+# something: {0}
+# """.format(value to change to string))
