71966daa53c78d5c8e9b8f0e3ef7d13485821b50
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-15 13:14:26 -0600

    slicing basics from strings

--- string_2.py
+++ string_2.py
@@ -1,17 +1,24 @@
-    #     01234567890123
-    #    -432109876543210            
+     #     01234567890123
+     #    -432109876543210            
 parrot = "Norwegian Blue"
-print(parrot)
-print(parrot[3])
-print(parrot[4]) # print(parrot[13])
-print(parrot[9])
-print(parrot[3])
-print(parrot[6])
-print(parrot[8])
-print(parrot[9])
-print(parrot[-11]) #print(3 -14)
-print(parrot[-1])
-print(parrot[-5])
-print(parrot[-11])
-print(parrot[-8])
-print(parrot[-6])
+# print(parrot)
+# print(parrot[3])
+# print(parrot[4]) # print(parrot[13])
+# print(parrot[9])
+# print(parrot[3])
+# print(parrot[6])
+# print(parrot[8])
+# print(parrot[9])
+# print(parrot[-11]) #print(3 -14)
+# print(parrot[-1])
+# print(parrot[-5])
+# print(parrot[-11])
+# print(parrot[-8])
+# print(parrot[-6])
+
+print(parrot[0:6])  # slice out up to locations six up to but not including index 6
+
+print(parrot[3:5])
+print(parrot[:8])#start defaults to index 0 and isn't needed.
+print(parrot[10:14])
+print(parrot[10:])
