a1cb5a309ca7656476733a06d70be434ddb1432b
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-15 15:11:58 -0600

    also works

--- slice_back.py
+++ slice_back.py
@@ -1,3 +1,4 @@
 letters = "abcdefghijklmnopqrstuvwxyz"
-backwards = letters[25::-1]  #default to end of string
+backwards = letters[::-1]  #default to end of string
+#backwards = letters[25::-1] #same
 print(backwards)
