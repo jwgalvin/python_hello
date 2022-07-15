1b7e202b1be0c45358c2de8f4b6c460aaaf48607
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-15 15:13:16 -0600

    more lsice back things

--- slice_back.py
+++ slice_back.py
@@ -2,3 +2,9 @@ letters = "abcdefghijklmnopqrstuvwxyz"
 backwards = letters[::-1]  #default to end of string
 #backwards = letters[25::-1] #same
 print(backwards)
+
+print(letters[16:13:-1])
+
+print(letters[4::-1])
+
+print(letters[:-9:-1])
