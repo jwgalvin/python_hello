1199d125a37e5357ad3010e055e2c4ca37c48e9c
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-15 15:53:21 -0600

    sequence operators added

---
+++ sequence_operators.py
@@ -0,0 +1,12 @@
+string1 = "he's "
+string2 = "probably "
+string3 = "pining "
+string4 = "for the"
+string5 = " fjords"
+print(string1 + string2 + string3 + string4 +string5)
+
+
+print(string1 * 5)
+
+today = "friday"
+print("day" in today) #true  returns boolean
