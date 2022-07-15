e9de9485c04facc8d1d655dc7641c5a5bab67254
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-15 11:14:13 -0600

    for loop

---
+++ operators.py
@@ -0,0 +1,13 @@
+a = 12
+b = 3
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+print(a // b)  #integer division rounded down
+print(a % b) #get the remainder
+
+print()
+
+for i in range(1, 4):
+    print(i)
