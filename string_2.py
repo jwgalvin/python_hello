95db1c9a9ccc1d6418371e5cbab4c99660ed0658
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-15 14:59:40 -0600

    slice in steps

--- string_2.py
+++ string_2.py
@@ -16,10 +16,17 @@ parrot = "Norwegian Blue"
 # print(parrot[-8])
 # print(parrot[-6])
 
-print(parrot[0:6])  # slice out up to locations six up to but not including index 6
+# print(parrot[0:6])  # slice out up to locations six up to but not including index 6
 
-print(parrot[3:5])
-print(parrot[:8])#start defaults to index 0 and isn't needed.
-print(parrot[10:14])
-print(parrot[10:])#starts at designated location and carries to end of string
-print(parrot[:]) #runs entire thing
+# print(parrot[3:5])
+# print(parrot[:8])#start defaults to index 0 and isn't needed.
+# print(parrot[10:14])
+# print(parrot[10:])#starts at designated location and carries to end of string
+# print(parrot[:]) #runs entire thing
+
+# print(parrot[-4:-2])
+# print(parrot[-4:12])# these are the same ^
+
+print(parrot[0:6:2]) #Nre  moves in steps of 2
+print(parrot[0:6:3]) #Nw   moves in steps of 3
+
