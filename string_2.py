2c416f064675b13d699c6c4155a824fc7a46e667
Joseph W Galvin <jwgalvin@gmail.com>
2022-07-15 13:05:18 -0600

    negative indexing used

--- string_2.py
+++ string_2.py
@@ -1,4 +1,5 @@
     #     01234567890123
+    #    -432109876543210            
 parrot = "Norwegian Blue"
 print(parrot)
 print(parrot[3])
@@ -7,4 +8,10 @@ print(parrot[9])
 print(parrot[3])
 print(parrot[6])
 print(parrot[8])
-
+print(parrot[9])
+print(parrot[-11])
+print(parrot[-1])
+print(parrot[-5])
+print(parrot[-11])
+print(parrot[-8])
+print(parrot[-6])
